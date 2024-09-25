971e8b0eb98a93d084524bbf258a0b2aebd2077e
Richard Burman <richard.burman@harperjames.co.uk>
2024-09-25 14:35:08 +0100

    Refactor user component to use dynamic image path and display selected user's avatar and name

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,22 +1,27 @@
-import { Component, Input } from '@angular/core';
+import { Component, computed, Input, input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input() avatar!: string;
-  @Input() name!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
 
   get imagePath() {
     return 'assets/users/' + this.avatar;
   }
 
-  onSelectUser() {
-    
-    
-  }
+// This is the more modern way to define inputs. 
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+
+  // imagePath = computed(() => {
+  //   return 'assets/users/' + this.avatar();
+  // });
+
+  onSelectUser() {}
 }
