84ec7589d159a5735e54dc765b6531ed789c944b
Richard Burman <richard.burman@harperjames.co.uk>
2024-10-23 15:22:08 +0100

    Refactor app.module.ts to add SharedModule and TasksModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
-import { TasksComponent } from './tasks/tasks.component';
 
-import { TaskComponent } from './tasks/task/task.component';
-import { NewTaskComponent } from './tasks/new-task/new-task.component';
-import { FormsModule } from '@angular/forms';
+
+
 import { SharedModule } from './shared/shared.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     UserComponent,
-    TasksComponent,
-    TaskComponent,
-    NewTaskComponent,
+    
   ], // Add the components here
   bootstrap: [AppComponent], // Add the root component here
-  imports: [BrowserModule, FormsModule, SharedModule], 
+  imports: [BrowserModule, SharedModule, TasksModule], 
 })
 export class AppModule {}
